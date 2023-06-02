e42a27909c9df379faabb1a2f361dcafdbf431d9
laura <ldiasvitaltoledo8@gmail.com>
2023-06-02 00:42:27 -0300

    Construção do C.R.U.D

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,7 @@
 import FetchUsers from './components/FetchUsers';
-import PostUsers from './components/PostUsers';
 
 export default function App() {
   return (
-    <div>
-      <PostUsers />,
       <FetchUsers />
-    </div>
   );
 }
