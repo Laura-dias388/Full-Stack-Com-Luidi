382bf3035e3652437459e69fe97ce001a4c9b4ec
laura <ldiasvitaltoledo8@gmail.com>
2023-06-22 02:22:10 -0300

    Deploy

--- backend/src/database/config/config.ts
+++ backend/src/database/config/config.ts
@@ -3,10 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config: Options = {
-  username: 'root',
-  password: 'password',
-  database: 'Estudos',
-  host: '127.0.0.1',// localhost
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  host: process.env.MYSQLHOST,// localhost
+  port: Number(process.env.MYSQLPORT),
   dialect: 'mysql',
 }
 
