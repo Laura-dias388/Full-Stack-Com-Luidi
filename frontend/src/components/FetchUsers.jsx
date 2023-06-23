a9a6e9a5e7bdae22d113f1256a0ba8d32d79ccb0
laura <ldiasvitaltoledo8@gmail.com>
2023-06-23 01:37:24 -0300

    Adiciona CSS

--- frontend/src/components/FetchUsers.jsx
+++ frontend/src/components/FetchUsers.jsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from 'react';
+import styles from '../styles/crudUsers.module.css';
 
 export default function FetchUsers() {
   // Tudo que está acima do return é javascript
   const INITIAL_PLACE = 'Digite seu nome';
   const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001';
 
-  const [usuarios, setUsuarios] = useState([]);
-  const [name, setName] = useState('');
-  const [isFetching, setIsFetching] = useState(true);
-  const [error, setError] = useState('');
-  const [placeholder, setPlaceholder] = useState(INITIAL_PLACE);
-  const [newUser, setNewUser] = useState({ id: '', userUpdated: '' });
+  const [ usuarios, setUsuarios ] = useState([]);
+  const [ name, setName ] = useState('');
+  const [ isFetching, setIsFetching ] = useState(true);
+  const [ error, setError ] = useState('');
+  const [ placeholder, setPlaceholder ] = useState(INITIAL_PLACE);
+  const [ newUser, setNewUser ] = useState({ id: '', userUpdated: '' });
 
   const fetchUsers = async () => {
     try {
@@ -58,7 +59,7 @@ export default function FetchUsers() {
   };
 
   const handleChange = ({ target }) => {
-    setNewUser((prevState) => ({ ...prevState, [target.name]: target.value }));
+    setNewUser((prevState) => ({ ...prevState, [ target.name ]: target.value }));
   };
 
   const updateUser = async (event) => {
@@ -78,68 +79,71 @@ export default function FetchUsers() {
   return ( // alterou estado o return é chamado
     // Tudo que está dentro do return é html,
     //  ao abrir um objeto dentro de uma tag é possivel usar javascript
-    <section>
-      <form onSubmit={postUser}>
-        <label htmlFor="name">
-            Name
-            <input
-              type="text"
-              id="name"
-              value={ name }
-              placeholder={placeholder}
-              onChange={ ({ target: { value } }) => setName(value) }/>
-        </label>
-        <button type="submit">Enviar</button>
-    </form>
-    <section>
-      {!isFetching ? (
-        <section>
-          { usuarios.length ? (
-            <section>
-              <table>
-                <thead>
-                  <tr>
-                    <th>Id</th>
-                    <th>Username</th>
-                    <th>Remover</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {usuarios.map((person, index) => (
-                    <tr key={index}>
-                      <td>{person.id}</td>
-                      <td>{person.name}</td>
-                      <td><button
-                        type="button"
-                        onClick={ () => deleteUser(person.id)}>Deletar</button></td>
+    <section className={ styles.sectionContainer }>
+      <section className={ styles.formContainer }>
+        <form className={ styles.form } onSubmit={ postUser }>
+          <h1>Novo Usuário</h1>
+          <input
+            type="text"
+            id="name"
+            value={ name }
+            className={ styles.input }
+            placeholder={ placeholder }
+            onChange={ ({ target: { value } }) => setName(value) } />
+          <button className={ styles.button } type="submit">Enviar</button>
+        </form>
+        <form className={ styles.form } onSubmit={ updateUser }>
+          <h2>Edite o usuário pelo id</h2>
+          <select className={ styles.styleSelect } name="id" id="" onChange={ handleChange }>
+            <option value="">Selecione Um Id</option>
+            { usuarios.map(({ id }, index) => (
+              <option key={ index } value={ id }>{ id }</option>
+            )) }
+          </select>
+          <input
+            type="text"
+            placeholder="Usuário á ser atualizado"
+            name="userUpdated"
+            className={ styles.input }
+            onChange={ handleChange } />
+          <button className={ styles.button } type="submit">Atualizar</button>
+        </form>
+      </section>
+      <section className={ styles.mainContainer }>
+        { !isFetching ? (
+          <section>
+            { usuarios.length ? (
+              <section className={ styles.tableContainer }>
+                <table className={ styles.table }>
+                  <thead>
+                    <tr>
+                      <th>Id</th>
+                      <th>Username</th>
+                      <th>Remover</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
-              <form onSubmit={updateUser}>
-                <h2>Edite o usuário pelo id</h2>
-                <select name="id" id="" onChange={handleChange}>
-                  <option value="">Selecione Um Id</option>
-                  {usuarios.map(({ id }, index) => (
-                    <option key={index} value={ id }>{id}</option>
-                  ))}
-                </select>
-                <input
-                  type="text"
-                  placeholder="Usuário á ser atualizado"
-                  name="userUpdated"
-                  onChange={handleChange}/>
-                <button type="submit">Atualizar</button>
-              </form>
-            </section>
-          ) : (
-            <span>{error}</span>
-          )}
-        </section>
-      ) : (
-        <span>Loading...</span>
-      )}
-    </section>
+                  </thead>
+                  <tbody>
+                    { usuarios.map((person, index) => (
+                      <tr key={ index }>
+                        <td>{ person.id }</td>
+                        <td>{ person.name }</td>
+                        <td><button
+                          type="button"
+                          className={ styles.tableDeletBtn }
+                          onClick={ () => deleteUser(person.id) }>Deletar</button></td>
+                      </tr>
+                    )) }
+                  </tbody>
+                </table>
+              </section>
+            ) : (
+              <span>{ error }</span>
+            ) }
+          </section>
+        ) : (
+          <span>Loading...</span>
+        ) }
+      </section>
     </section>
   );
 }
